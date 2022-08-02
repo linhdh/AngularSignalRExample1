33a0548f13a7737c26698894f9ba2b036e596f0b
Dao Hai Linh <linhdh@live.com>
2022-08-02 13:36:34 +0700

    Add Employees controller.

---
+++ AngularSignalRExample1/WebApi/Controllers/EmployeesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly IHubContext<BroadcastHub, IHubClient> _hubContext;
+
+        public EmployeesController(AppDbContext context, IHubContext<BroadcastHub, IHubClient> hubContext)
+        {
+            _context = context;
+            _hubContext = hubContext;
+        }
+
+        // GET: api/Employees
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+        {
+            if (_context.Employees == null)
+            {
+                return NotFound();
+            }
+            return await _context.Employees.ToListAsync();
+        }
+
+        // GET: api/Employees/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployee(Guid id)
+        {
+            if (_context.Employees == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employees.FindAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return employee;
+        }
+
+        // PUT: api/Employees/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmployee(Guid id, Employee employee)
+        {
+            if (id != employee.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(employee).State = EntityState.Modified;
+
+            Notification notification = new Notification()
+            {
+                EmployeeName = employee.Name,
+                TranType = "Edit"
+            };
+
+            await _context.Notifications.AddAsync(notification);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+                await _hubContext.Clients.All.BroadcastMessage();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Employees
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Employee>> PostEmployee(Employee employee)
+        {
+            if (_context.Employees == null)
+            {
+                return Problem("Entity set 'AppDbContext.Employees'  is null.");
+            }
+            _context.Employees.Add(employee);
+            
+            Notification notification = new Notification()
+            {
+                EmployeeName = employee.Name,
+                TranType = "Add"
+            };
+            _context.Notifications.Add(notification);
+
+            await _context.SaveChangesAsync();
+            await _hubContext.Clients.All.BroadcastMessage();
+
+            return CreatedAtAction("GetEmployee", new { id = employee.Id }, employee);
+        }
+
+        // DELETE: api/Employees/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(Guid id)
+        {
+            if (_context.Employees == null)
+            {
+                return NotFound();
+            }
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+
+            Notification notification = new Notification()
+            {
+                EmployeeName = employee.Name,
+                TranType = "Delete"
+            };
+            _context.Notifications.Add(notification);
+            await _hubContext.Clients.All.BroadcastMessage();
+
+            return NoContent();
+        }
+
+        private bool EmployeeExists(Guid id)
+        {
+            return (_context.Employees?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
