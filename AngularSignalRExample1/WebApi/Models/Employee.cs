9c85fa5b5e470fb8c63461df1129f7a877f425dc
Dao Hai Linh <linhdh@live.com>
2022-08-02 10:03:46 +0700

    Add Employee model.

--- AngularSignalRExample1/WebApi/Models/Employee.cs
+++ AngularSignalRExample1/WebApi/Models/Employee.cs
@@ -2,5 +2,12 @@
 {
     public class Employee
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public string Company { get; set; }
+        public string Cityname { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
     }
 }
