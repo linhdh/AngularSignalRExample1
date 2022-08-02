1989e348765fbf22e28e472b7df6e4ebe8c850a5
Dao Hai Linh <linhdh@live.com>
2022-08-02 16:52:03 +0700

    Add modal component

--- Frontend/AngularSignalR/src/app/app.module.ts
+++ Frontend/AngularSignalR/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
 import { EmployeeEditComponent } from './employee/employee-edit/employee-edit.component';
 import { EmployeeDetailComponent } from './employee/employee-detail/employee-detail.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeListComponent,
     EmployeeEditComponent,
-    EmployeeDetailComponent
+    EmployeeDetailComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
